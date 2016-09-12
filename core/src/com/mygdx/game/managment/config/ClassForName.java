ddab115433725c5c6159b3b551ac71a4e5f434fc
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-09-12 13:56:06 +0300

    Added documentation

--- core/src/com/mygdx/game/managment/config/ClassForName.java
+++ core/src/com/mygdx/game/managment/config/ClassForName.java
@@ -6,27 +6,34 @@ import com.mygdx.game.sprites.frogs.RegularFrog;
 import com.mygdx.game.sprites.frogs.YellowFrog;
 
 /**
+ * This class is used to translate a given class-name to it's corresponding class-object.
+ *
  * Created by MichaelBond on 9/10/2016.
  */
 public class ClassForName {
     // Frog's classes names.
     private static final String REGULAR_FROG = RegularFrog.class.getCanonicalName();
-    private static final String Red_Frog = RedFrog.class.getCanonicalName();
-    private static final String Yellow_Frog = YellowFrog.class.getCanonicalName();
+    private static final String RED_FROG = RedFrog.class.getCanonicalName();
+    private static final String YELLOW_FROG = YellowFrog.class.getCanonicalName();
 
 
+    /**
+     * Translates a class-name to it's corresponding object.
+     *
+     * @param classNme  The name of tha class we wish to get back.
+     * @return  The class object with the given class-name.
+     * @throws ClassNotFoundException   In case no class was found for the given class-name.
+     */
     public static Class<? extends Frog> getFrogClassByName(String classNme)
             throws ClassNotFoundException{
 
         if (REGULAR_FROG.equals(classNme)) {
             return RegularFrog.class;
         }
-        else
-        if (Red_Frog.equals(classNme)) {
+        else if (RED_FROG.equals(classNme)) {
             return RedFrog.class;
         }
-        else
-        if (Yellow_Frog.equals(classNme)) {
+        else if (YELLOW_FROG.equals(classNme)) {
             return YellowFrog.class;
         }
 
