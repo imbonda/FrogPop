8d91d5e2b5d65b4052dd9d3c488f1dd6c7abb4d2
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-09-12 14:23:37 +0300

    Added documentation

--- core/src/com/mygdx/game/managment/FrogManager.java
+++ core/src/com/mygdx/game/managment/FrogManager.java
@@ -16,13 +16,15 @@ import com.mygdx.game.sprites.Hole;
 
 
 /**
+ * This class is responsible for managing all the (active frogs) on the screen.
+ *
  * Created by MichaelBond on 8/28/2016.
  */
 public class FrogManager {
 
     public Array<Frog> activeFrogs;
 
-    private static float FROG_LIFE_TIME_SECS = 5.0f;
+    private static final float FROG_LIFE_TIME_SECS = 5.0f;
     private static final int FROG_OFFSET_X = 55;
     private static final int FROG_OFFSET_Y = 20;
 
@@ -45,6 +47,9 @@ public class FrogManager {
         }
     }
 
+    /**
+     * Adds a frog to pop on the screen.
+     */
     public void addFrog() {
         int randomHoleIndex = this.getRandomUnpopulatedHoleIndex();
         Vector2 frogPosition = this.getFrogPlacementPosition(randomHoleIndex);
@@ -62,6 +67,11 @@ public class FrogManager {
         }
     }
 
+    /**
+     * Used for fetching a random unpopulated hole index.
+     *
+     * @return  A random unpopulated hole index.
+     */
     private int getRandomUnpopulatedHoleIndex() {
         Integer unpopulatedHoleIndex = this.unpopulatedHolesIndexes.random();
         if (null == unpopulatedHolesIndexes) {
@@ -80,6 +90,11 @@ public class FrogManager {
         this.frogMaxLifeTime *= decreaseFactor;
     }
 
+    /**
+     * Updates all the (active) frog on the screen.
+     *
+     * @param deltaTime The time passed from the last call to update.
+     */
     public void update(float deltaTime) {
         Iterator<Frog> frogIterator = this.activeFrogs.iterator();
         while (frogIterator.hasNext()) {
@@ -96,6 +111,12 @@ public class FrogManager {
         }
     }
 
+    /**
+     * Replaces an already dead frog with a new one.
+     *
+     * @param frogIterator  A frog-iterator to be used in order to remove the given frog.
+     * @param frog  The frog to be removed from the screen.
+     */
     private void recycleDeadFrog(Iterator<Frog> frogIterator, Frog frog) {
         int frogHoleIndex = this.frogToHoleIndexMap.remove(frog);
         frogIterator.remove();
