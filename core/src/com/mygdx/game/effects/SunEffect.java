0f0461224c0989d9dca6f143a9557130e7678d7a
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-09-27 17:44:17 +0300

    Updated sun effect

--- core/src/com/mygdx/game/effects/SunEffect.java
+++ core/src/com/mygdx/game/effects/SunEffect.java
@@ -8,21 +8,37 @@ import java.util.Random;
  * Created by nitsa on 27-Sep-16.
  */
 public class SunEffect {
-    private Texture sun=new Texture("sun.jpg");
-    int speed1 = 8;
+    private final Texture sunTexture[] = {
+            new Texture("sun0.png"),
+            new Texture("sun1.png"),
+            new Texture("sun2.png"),
+            new Texture("sun3.png"),
+            new Texture("sun4.png"),
+            new Texture("sun5.png"),
+            new Texture("sun6.png"),
+    };
+    private double i = 0;
     int delay=1;
-    private int sunpos=0;
+    private int sunpos=380;
+    private double dir = 0.25;
 
     public SunEffect() {
     }
     private void postition(){
-        sunpos+=-speed1;
+        if (i==0){
+            dir=0.25;
+        }
+        if(i>6.6){
+            i=0;
+        }
+        i+=dir;
     }
 
     public void draw(Batch batch){
         delay++;
-        if(delay%2==0){
+
+        if(delay%6==0){
         postition();}
-        batch.draw(sun,sunpos,365);
+        batch.draw(sunTexture[(int)i],sunpos,460);
     }
 }
