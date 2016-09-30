125f29c84e67d3c00dbaa72a32007d1d55b45a47
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-09-30 16:20:58 +0300

    Added DummyAdsController to desktop

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,12 +3,14 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.FrogPop;
+import com.mygdx.game.adds.AdsController;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width=1366;
 		config.height=768;
-		new LwjglApplication(new FrogPop(), config);
+		new LwjglApplication(new FrogPop(new DummyAdsController()), config);
 	}
 }
