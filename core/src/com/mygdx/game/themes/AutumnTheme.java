a0f2531f9947093c0357b121e8c2bab5ceaae2b9
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-09-30 23:19:18 +0300

    Using updated themes images and spring with leaves

--- core/src/com/mygdx/game/themes/AutumnTheme.java
+++ core/src/com/mygdx/game/themes/AutumnTheme.java
@@ -24,9 +24,15 @@ public class AutumnTheme implements Theme {
 
     private Texture backgroundTexture;
     private RainEffect rainEffect;
+    private Texture leaves[];
 
     public AutumnTheme() {
         this.backgroundTexture = new Texture("autumn.png");
+        this.leaves = new Texture[] {
+                    new Texture("leave.png"),
+                    new Texture("leave2.png"),
+                    new Texture("leave3.png")
+        };
         this.rainEffect = new RainEffect();
     }
 
@@ -39,6 +45,18 @@ public class AutumnTheme implements Theme {
     public void draw(Batch batch) {
         Gdx.gl.glClearColor(171/255f,107/255f,72/255f,1);
         batch.draw(this.backgroundTexture, 0, 0);
+        batch.draw(leaves[0],10,20);
+        batch.draw(leaves[0],100,20);
+        batch.draw(leaves[0],100,200);
+        batch.draw(leaves[1],300,330);
+        batch.draw(leaves[1],400,120);
+        batch.draw(leaves[1],550,100);
+        batch.draw(leaves[2],650,170);
+        batch.draw(leaves[2],740,20);
+        batch.draw(leaves[2],220,20);
+        batch.draw(leaves[1],650,300);
+        batch.draw(leaves[2],700,250);
+        batch.draw(leaves[0],10,70);
         this.rainEffect.draw(batch);
     }
 
