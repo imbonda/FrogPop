a3d193f519ab96d235110e169674eb86ed050844
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-10-01 20:15:44 +0300

    Added percentage in settings sliders

--- core/src/com/mygdx/game/screens/SettingsScreen.java
+++ core/src/com/mygdx/game/screens/SettingsScreen.java
@@ -26,6 +26,7 @@ public class SettingsScreen implements Screen {
 
     private static final String MUSIC = "Music";
     private static final String SOUND = "Sound";
+    private static final String PERCENTAGE = "%";
     private static final int SLIDER_RANGE = 10;
     private static final int SLIDER_STEP = 1;
 
@@ -33,6 +34,8 @@ public class SettingsScreen implements Screen {
 //    private Texture soundTexture;
     private Label musicLabel;
     private Label soundLabel;
+    private Label musicSliderLable;
+    private Label soundSliderLable;
     private FrogPop game;
     private Slider musicSlider;
     private Slider soundSlider;
@@ -50,15 +53,17 @@ public class SettingsScreen implements Screen {
         this.musicLabel.setWidth(250);
         this.musicLabel.setColor(1, 1, 1, 1);
         // Music slider.
+        float musicVolume = game.data.getMusicVolume();
         this.musicSlider = new Slider(0, SLIDER_RANGE, SLIDER_STEP, false, slideSkin);
         this.musicSlider.setPosition(350, 300);
         this.musicSlider.setAnimateDuration(0);
-        this.musicSlider.setValue(game.data.getMusicVolume() * SLIDER_RANGE);
+        this.musicSlider.setValue(musicVolume * SLIDER_RANGE);
         this.musicSlider.addListener(new ChangeListener() {
             @Override
             public void changed (ChangeEvent event, Actor actor) {
                 float newMusicVolume = musicSlider.getValue() / SLIDER_RANGE;
                 updateMedia(newMusicVolume);
+                updateSliderLabel(newMusicVolume);
             }
 
             private void updateMedia(float volume) {
@@ -71,7 +76,17 @@ public class SettingsScreen implements Screen {
                     game.media.playMusic();
                 }
             }
+
+            private void updateSliderLabel(float volume) {
+                musicSliderLable.setText(Integer.toString((int)(volume * 100)) + PERCENTAGE);
+            }
         });
+        // Music-slider label.
+        this.musicSliderLable = new Label(Integer.toString((int)(musicVolume * 100)) + PERCENTAGE,
+                    labelStyle);
+        this.musicSliderLable.setPosition(520, 300);
+        this.musicSliderLable.setWidth(100);
+        this.musicSliderLable.setColor(1, 1, 1, 1);
 
         // Sound label.
         this.soundLabel = new Label(SOUND, labelStyle);
@@ -79,30 +94,44 @@ public class SettingsScreen implements Screen {
         this.soundLabel.setWidth(250);
         this.soundLabel.setColor(1, 1, 1, 1);
         // Sound slider.
+        float soundVolume = game.data.getSoundVolume();
         this.soundSlider = new Slider(0, SLIDER_RANGE, SLIDER_STEP, false, slideSkin);
         this.soundSlider.setPosition(350, 200);
         this.soundSlider.setAnimateDuration(0);
-        this.soundSlider.setValue(game.data.getSoundVolume() * SLIDER_RANGE);
+        this.soundSlider.setValue(soundVolume * SLIDER_RANGE);
         this.soundSlider.addListener(new ChangeListener() {
             @Override
             public void changed (ChangeEvent event, Actor actor) {
                 float newSoundVolume = soundSlider.getValue() / SLIDER_RANGE;
                 updateMedia(newSoundVolume);
+                updateSliderLabel(newSoundVolume);
             }
 
             private void updateMedia(float volume) {
                 game.data.setSoundVolume(volume);
                 game.media.updateSoundVolume(volume);
             }
+
+            private void updateSliderLabel(float volume) {
+                soundSliderLable.setText(Integer.toString((int)(volume * 100)) + PERCENTAGE);
+            }
         });
+        // Sound-slider label.
+        this.soundSliderLable = new Label(Integer.toString((int)(soundVolume * 100)) + PERCENTAGE,
+                    labelStyle);
+        this.soundSliderLable.setPosition(520, 200);
+        this.soundSliderLable.setWidth(100);
+        this.soundSliderLable.setColor(1, 1, 1, 1);
 
         this.stage = new Stage(
                 new FitViewport(FrogPop.VIRTUAL_WIDTH, FrogPop.VIRTUAL_HEIGHT, new OrthographicCamera()),
                 this.game.batch);
         this.stage.addActor(musicLabel);
         this.stage.addActor(musicSlider);
+        this.stage.addActor(musicSliderLable);
         this.stage.addActor(soundLabel);
         this.stage.addActor(soundSlider);
+        this.stage.addActor(soundSliderLable);
         Gdx.input.setInputProcessor(this.stage);
         Gdx.gl.glClearColor(0, 1, 1, 1);
     }
