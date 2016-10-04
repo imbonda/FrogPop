20b12b97103a9af7329ee13725b70408db0fc9f2
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-10-05 00:24:41 +0300

    Using nitans updated butterfly effect

--- core/src/com/mygdx/game/effects/ButterflyEffect.java
+++ core/src/com/mygdx/game/effects/ButterflyEffect.java
@@ -8,160 +8,162 @@ import java.util.Random;
 /**
  * Created by nitsa on 27-Sep-16.
  */
-public class ButterflyEffect implements Effect {
+public class ButterflyEffect {
+    private static final float UPDATE_SPEED=0.05f;
+    private static final int RIGHT_PARAMETER=5;
+    private static final int LEFT_PARAMETER=2;
+    private float speed=0;
+    private Texture[] butterfly={new Texture("butter.png"),new Texture("butter2.png"),new Texture("butter3.png")};
+    private Random rand = new Random();
+    private Vector2 butterfly1pos=new Vector2(70,465);
+    private Vector2 butterfly2pos=new Vector2(200,340);
+    private Vector2 butterfly3pos=new Vector2(300,520);
+    private Vector2 butterfly4pos=new Vector2(450,200);
+    private Vector2 butterfly5pos=new Vector2(570,170);
+    private Vector2 butterfly6pos=new Vector2(640,80);
+    private Vector2 speed1 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+    private Vector2 speed2 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+    private Vector2 speed3 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+    private Vector2 speed4 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+    private Vector2 speed5 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+    private Vector2 speed6 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
 
-    private Texture butter1=new Texture("butter.png");
-    private Texture butter2=new Texture("butter2.png");
-    private Texture butter3=new Texture("Butter3.png");
-
-    Vector2 speed1 = new Vector2(1,1);
-    Vector2 speed2 = new Vector2(1,1);
-    Vector2 speed3 = new Vector2(1,1);
-    int delay=1;
-    private Vector2 cloud1pos=new Vector2(500,365);
-    private Vector2 cloud2pos=new Vector2(100,365);
-    private Vector2 cloud3pos=new Vector2(200,365);
-    private Vector2 cloud4pos=new Vector2(300,365);
-    private Vector2 cloud5pos=new Vector2(400,365);
-    private Vector2 cloud6pos=new Vector2(660,365);
     public ButterflyEffect() {
-        Random rand = new Random();
-
     }
     private void postition(){
         Random rand = new Random();
-        speed1 =new Vector2(rand.nextInt(4)-2,rand.nextInt(8)-2);
-        speed2 =new Vector2(rand.nextInt(4)-2,rand.nextInt(4)-2);
-        speed3 =new Vector2(rand.nextInt(6)-2,rand.nextInt(6)-4);
-        cloud1pos.add(speed1.x,speed2.y);
-        cloud2pos.add(speed1.x,speed3.y);
-        cloud3pos.add(speed2.x,speed1.y);
-        cloud4pos.add(speed2.x,speed3.y);
-        cloud5pos.add(speed3.x,speed1.y);
-        cloud6pos.add(speed3.x,speed2.y);
-        if(speed1.x>0||speed2.x>0||speed3.x>0){
-        if(cloud1pos.x>800)
+        speed1 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+        speed2 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+        speed3 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+        speed4 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+        speed5 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+        speed6 =new Vector2(rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER,rand.nextInt(RIGHT_PARAMETER)-LEFT_PARAMETER);
+        if(butterfly1pos.x>770)
         {
-            cloud1pos.x=0;
+            speed1.x=-1*speed1.x;
         }
-        if(cloud2pos.x>800)
+        if(butterfly2pos.x>770)
         {
-            cloud2pos.x=0;
+            speed2.x=-1*speed2.x;
         }
-        if(cloud3pos.x>800)
+        if(butterfly3pos.x>770)
         {
-            cloud3pos.x=0;
+            speed3.x=-1*speed3.x;
         }
-        if(cloud4pos.x>800)
+        if(butterfly4pos.x>770)
         {
-            cloud4pos.x=0;
+            speed4.x=-1*speed4.x;
         }
-        if(cloud5pos.x>800)
+        if(butterfly5pos.x>770)
         {
-            cloud5pos.x=0;
+            speed5.x=-1*speed5.x;
         }
-        if(cloud6pos.x>800)
+        if(butterfly6pos.x>770)
         {
-            cloud6pos.x=0;
-        }}
-        if(speed1.x<0||speed2.x<0||speed3.x<0){
-            if(cloud1pos.x<0)
-            {
-                cloud1pos.x=800;
-            }
-            if(cloud2pos.x<00)
-            {
-                cloud2pos.x=800;
-            }
-            if(cloud3pos.x<0)
-            {
-                cloud3pos.x=800;
-            }
-            if(cloud4pos.x<0)
-            {
-                cloud4pos.x=800;
-            }
-            if(cloud5pos.x<0)
-            {
-                cloud5pos.x=800;
-            }
-            if(cloud6pos.x<0)
-            {
-                cloud6pos.x=800;
-            }}
-        if(speed1.y>0||speed2.y>0||speed3.y>0){
-            if(cloud1pos.y>530)
-            {
-                cloud1pos.y=0;
-            }
-            if(cloud2pos.y>530)
-            {
-                cloud2pos.y=0;
-            }
-            if(cloud3pos.y>530)
-            {
-                cloud3pos.y=0;
-            }
-            if(cloud4pos.y>530)
-            {
-                cloud4pos.y=0;
-            }
-            if(cloud5pos.y>530)
-            {
-                cloud5pos.x=0;
-            }
-            if(cloud6pos.y>530)
-            {
-                cloud6pos.y=0;
-            }}
-        if(speed1.y<0||speed2.y<0||speed3.y<0){
-            if(cloud1pos.y<0)
-            {
-                cloud1pos.y=530;
-            }
-            if(cloud2pos.y<0)
-            {
-                cloud2pos.y=530;
-            }
-            if(cloud3pos.y<0)
-            {
-                cloud3pos.y=530;
-            }
-            if(cloud4pos.y<0)
-            {
-                cloud4pos.y=530;
-            }
-            if(cloud5pos.y<0)
-            {
-                cloud5pos.y=530;
-            }
-            if(cloud6pos.y<0)
-            {
-                cloud6pos.y=530;
-            }}
-    }
+            speed6.x=-1*speed6.x;
+        }
 
-    @Override
+        if(butterfly1pos.x<0)
+        {
+            speed1.x=-1*speed1.x;
+        }
+        if(butterfly2pos.x<0)
+        {
+            speed2.x=-1*speed2.x;
+        }
+        if(butterfly3pos.x<0)
+        {
+            speed3.x=-1*speed3.x;
+        }
+        if(butterfly4pos.x<0)
+        {
+            speed4.x=-1*speed4.x;
+        }
+        if(butterfly5pos.x<0)
+        {
+            speed5.x=-1*speed5.x;
+        }
+        if(butterfly6pos.x<0)
+        {
+            speed6.x=-1*speed6.x;
+        }
+
+        if(butterfly1pos.y>530)
+        {
+            speed1.y=-1*speed1.y;
+        }
+        if(butterfly2pos.y>530)
+        {
+            speed2.y=-1*speed2.y;
+        }
+        if(butterfly3pos.y>530)
+        {
+            speed3.y=-1*speed3.y;
+        }
+        if(butterfly4pos.y>530)
+        {
+            speed4.y=-1*speed4.y;
+        }
+        if(butterfly5pos.y>530)
+        {
+            speed5.y=-1*speed5.y;
+        }
+        if(butterfly6pos.y>530)
+        {
+            speed6.y=-1*speed6.y;
+        }
+        if(butterfly1pos.y<0)
+        {
+            speed1.y=-1*speed1.y;
+        }
+        if(butterfly2pos.y<0)
+        {
+            speed2.y=-1*speed2.y;
+        }
+        if(butterfly3pos.y<0)
+        {
+            speed3.y=-1*speed3.y;
+        }
+        if(butterfly4pos.y<0)
+        {
+            speed4.y=-1*speed4.y;
+        }
+        if(butterfly5pos.y<0)
+        {
+            speed5.y=-1*speed5.y;
+        }
+        if(butterfly6pos.y<0)
+        {
+            speed6.y=-1*speed6.y;
+        }
+        // speed1.scl(3,3f);
+        // speed2.scl(3,3);
+        // speed3.scl(3,3);
+        // speed4.scl(3,3);
+        // speed5.scl(3,3);
+        // speed6.scl(3,3);
+        System.out.println(speed1.x);
+
+    }
     public void update(float deltaTime) {
-        // Nothing to update.
-    }
-
-    @Override
-    public void draw(Batch batch){
-        delay++;
-        if(delay%2==0) {
+        speed+=deltaTime;
+        butterfly1pos.add(speed1.x,speed1.y);
+        butterfly2pos.add(speed2.x,speed2.y);
+        butterfly3pos.add(speed3.x,speed3.y);
+        butterfly4pos.add(speed4.x,speed4.y);
+        butterfly5pos.add(speed5.x,speed5.y);
+        butterfly6pos.add(speed6.x,speed6.y);
+        if(speed>UPDATE_SPEED){
             postition();
-        }
-        batch.draw(butter1,cloud1pos.x,cloud1pos.y);
-        batch.draw(butter1,cloud2pos.x,cloud2pos.y);
-        batch.draw(butter2,cloud3pos.x,cloud3pos.y);
-        batch.draw(butter2,cloud4pos.x,cloud4pos.y);
-        batch.draw(butter3,cloud5pos.x,cloud5pos.y);
-        batch.draw(butter3,cloud6pos.x,cloud6pos.y);
-    }
+            speed=0;
+        }}
 
-    @Override
-    public void reset() {
-        // Nothing to reset.
+    public void draw(Batch batch){
+        batch.draw(butterfly[0],butterfly1pos.x,butterfly1pos.y);
+        batch.draw(butterfly[1],butterfly2pos.x,butterfly2pos.y);
+        batch.draw(butterfly[2],butterfly3pos.x,butterfly3pos.y);
+        batch.draw(butterfly[0],butterfly4pos.x,butterfly4pos.y);
+        batch.draw(butterfly[1],butterfly5pos.x,butterfly5pos.y);
+        batch.draw(butterfly[2],butterfly6pos.x,butterfly6pos.y);
     }
 }
