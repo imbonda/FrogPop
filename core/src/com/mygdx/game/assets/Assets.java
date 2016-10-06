5cc8707dd3eeb3a3a507c9f2e33a4d5a1b62460d
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-10-06 15:40:43 +0300

    Added all special frogs to Assets class

---
+++ core/src/com/mygdx/game/assets/Assets.java
@@ -0,0 +1,130 @@
+package com.mygdx.game.assets;
+
+/**
+ * Created by MichaelBond on 10/6/2016.
+ */
+public class Assets {
+
+    /**
+     * Media.
+     */
+    // Music.
+    public static final String GAME_PLAY_MUSIC = "media/music.ogg";
+    // Sound.
+    public static final String LEVEL_UP_SOUND = "media/level_up.wav";
+    public static final String GAME_OVER_SOUND = "media/endgame.mp3";
+
+    public static final String MUSIC_FILES [] = {
+            GAME_PLAY_MUSIC
+    };
+    public static final String SOUND_FILES [] = {
+            LEVEL_UP_SOUND,
+            GAME_OVER_SOUND
+    };
+
+
+    /**
+     * Textures.
+     */
+    // Themes.
+    public static final String SUMMER_THEME = "images/themes/summer.png";
+    public static final String AUTUMN_THEME = "images/themes/autumn.png";
+    public static final String WINTER_THEME = "images/themes/winter.png";
+    public static final String SPRING_THEME = "images/themes/spring.png";
+    // Sprites.
+    public static final String TIMER = "images/timer.png";
+    public static final String CLOUD = "images/cloud.png";
+    // Butterflies.
+    public static final String BUTTERFLY_ORANGE = "images/butterflies/orange.png";
+    public static final String BUTTERFLY_PURPLE = "images/butterflies/purple.png";
+    public static final String BUTTERFLY_RED = "images/butterflies/red.png";
+    // Bird textures.
+    public static final String BIRD1 = "images/bird/bird1.png";
+    public static final String BIRD2 = "images/bird/bird2.png";
+    public static final String BIRD3 = "images/bird/bird3.png";
+
+    // Health frog.
+    public static final String HEALTH_FROG1 = "images/frogs/health/frog1.png";
+    public static final String HEALTH_FROG2 = "images/frogs/health/frog2.png";
+    // Evil frog.
+    public static final String EVIL_FROG1 = "images/frogs/evil/frog1.png";
+    public static final String EVIL_FROG2 = "images/frogs/evil/frog2.png";
+    public static final String EVIL_FROG3 = "images/frogs/evil/frog3.png";
+    // Freeze frog.
+    public static final String FREEZE_FROG1 = "images/frogs/freeze/frog1.png";
+    public static final String FREEZE_FROG2 = "images/frogs/freeze/frog2.png";
+    public static final String FREEZE_FROG3 = "images/frogs/freeze/frog3.png";
+    // Illusion frog.
+    public static final String ILLUSION_FROG1 = "images/frogs/illusion/frog1.png";
+    public static final String ILLUSION_FROG2 = "images/frogs/illusion/frog2.png";
+    public static final String ILLUSION_FROG3 = "images/frogs/illusion/frog3.png";
+    public static final String ILLUSION_FROG4 = "images/frogs/illusion/frog4.png";
+    public static final String ILLUSION_FROG5 = "images/frogs/illusion/frog5.png";
+    public static final String ILLUSION_FROG6 = "images/frogs/illusion/frog6.png";
+    public static final String ILLUSION_FROG7 = "images/frogs/illusion/frog7.png";
+
+    public static final String TEXTURE_FILES [] = {
+        // Themes
+            SUMMER_THEME,
+            AUTUMN_THEME,
+            WINTER_THEME,
+            SPRING_THEME,
+        // Sprites
+            TIMER,
+            CLOUD,
+            BUTTERFLY_ORANGE,
+            BUTTERFLY_PURPLE,
+            BUTTERFLY_RED,
+        // Animation textures
+            BIRD1, BIRD2, BIRD3,
+            HEALTH_FROG1, HEALTH_FROG2,
+            EVIL_FROG1, EVIL_FROG2, EVIL_FROG3,
+            ILLUSION_FROG1, ILLUSION_FROG2, ILLUSION_FROG3, ILLUSION_FROG4, ILLUSION_FROG5,
+            ILLUSION_FROG6, ILLUSION_FROG7,
+
+    };
+
+
+    /**
+     * Animations textures.
+     */
+    public static final String BIRD_ANIMATION [] = {
+            BIRD1,
+            BIRD2,
+            BIRD3,
+            BIRD2
+    };
+    public static final String HEALTH_FROG_ANIMATION [] = {
+            HEALTH_FROG1,
+            HEALTH_FROG2
+    };
+    public static final String EVIL_FROG_ANIMATION [] = {
+            EVIL_FROG1,
+            EVIL_FROG2,
+            EVIL_FROG3,
+            EVIL_FROG3
+    };
+    public static final String FREEZE_FROG_ANIMATION [] = {
+            FREEZE_FROG1,
+            FREEZE_FROG2,
+            FREEZE_FROG3,
+            FREEZE_FROG3,
+            FREEZE_FROG3,
+            FREEZE_FROG2
+    };
+    public static final String ILLUSION_FROG_ANIMATION [] = {
+            ILLUSION_FROG1,
+            ILLUSION_FROG2,
+            ILLUSION_FROG3,
+            ILLUSION_FROG4,
+            ILLUSION_FROG3,
+            ILLUSION_FROG2,
+            ILLUSION_FROG1,
+            ILLUSION_FROG5,
+            ILLUSION_FROG6,
+            ILLUSION_FROG7,
+            ILLUSION_FROG6,
+            ILLUSION_FROG5
+    };
+
+}
