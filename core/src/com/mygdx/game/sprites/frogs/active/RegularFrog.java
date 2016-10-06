a2ea2e8b207efb6b5423766499f7c003548198e6
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-10-06 20:28:30 +0300

    Drawing regular frog using AssetController

--- core/src/com/mygdx/game/sprites/frogs/active/RegularFrog.java
+++ core/src/com/mygdx/game/sprites/frogs/active/RegularFrog.java
@@ -4,8 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
-import com.mygdx.game.animation.CAnimation;
+import com.mygdx.game.animation.Animation;
 import com.mygdx.game.assets.AssetController;
+import com.mygdx.game.assets.Assets;
 import com.mygdx.game.runtime.RuntimeInfo;
 
 import java.util.Random;
@@ -26,38 +27,8 @@ public class RegularFrog extends Frog {
     private static final int TONGUE_ANIMATION = 0;
     private static final int WINK_ANIMATION = 1;
 
-    private final Texture tongueAnimationTextures [] = {
-            new Texture("Frog/0.png"),
-            new Texture("Frog/1.png"),
-            new Texture("Frog/2.png"),
-            new Texture("Frog/3.png"),
-    };
-    private final Texture winkAnimationTextures [] = {
-            new Texture("Frog/0.png"),
-            new Texture("Frog/eye2.png"),
-            new Texture("Frog/eye3.png"),
-            new Texture("Frog/eye4.png")
-    };
-    private final Texture britishAnimationTextures [] = {
-            new Texture("Frog/special/britain1.png"),
-            new Texture("Frog/special/britain2.png"),
-            new Texture("Frog/special/britain3.png"),
-            new Texture("Frog/special/britain2.png"),
-    };
-    private final Texture turkAnimationTextures [] = {
-            new Texture("Frog/special/turk1.png"),
-            new Texture("Frog/special/turk2.png"),
-            new Texture("Frog/special/turk3.png"),
-            new Texture("Frog/special/turk2.png"),
-    };
-    private final Texture mexicanAnimationTextures [] = {
-            new Texture("Frog/special/mex1.png"),
-            new Texture("Frog/special/mex2.png"),
-            new Texture("Frog/special/mex1.png"),
-            new Texture("Frog/special/mex2.png"),
-    };
 
-    private CAnimation animation;
+    private Animation animation;
 
 
     public RegularFrog() {
@@ -66,24 +37,17 @@ public class RegularFrog extends Frog {
     private void generateRandomAnimation() {
         Random rand = new Random();
         if (rand.nextInt(2) == TONGUE_ANIMATION) {
-            this.animation = new CAnimation(tongueAnimationTextures);
-            setSize(tongueAnimationTextures[0].getWidth(), tongueAnimationTextures[0].getHeight());
+            this.animation = this.assetController.getAnimation(Assets.HERO_REGULAR_TONGUE_ANIMATION);
         }
         else {
-            this.animation = new CAnimation(winkAnimationTextures);
-            setSize(winkAnimationTextures[0].getWidth(), winkAnimationTextures[0].getHeight());
+            this.animation = this.assetController.getAnimation(Assets.HERO_REGULAR_WINK_ANIMATION);
         }
+        Texture frame = this.animation.getFrame();
+        setSize(frame.getWidth(), frame.getHeight());
     }
 
     @Override
     public void dispose() {
-        // TODO (check if we want this behavior).
-        for (Texture texture : this.tongueAnimationTextures) {
-            texture.dispose();
-        }
-        for (Texture texture : this.winkAnimationTextures) {
-            texture.dispose();
-        }
     }
 
     @Override
@@ -126,7 +90,7 @@ public class RegularFrog extends Frog {
     @Override
     public void draw(Batch batch) {
         batch.draw(getFrogTexture(), this.position.x, this.position.y,
-                0, 0, 100, 100-(int)(((FROG_MAX_LIFE_TIME - this.lifeTime)*100)/(FROG_MAX_LIFE_TIME)));
+                0, 0, (int)getWidth(), (int)getHeight()-(int)(((FROG_MAX_LIFE_TIME - this.lifeTime)*100)/(FROG_MAX_LIFE_TIME)));
     }
 
     public Texture getFrogTexture() {
