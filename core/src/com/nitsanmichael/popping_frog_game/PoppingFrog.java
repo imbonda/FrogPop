07fa7ef305fe064bc2c20275fce3625b25442d83
Michael Bondarevsky <michealbnd1993@gmail.com>
2016-11-06 02:42:32 +0200

    Removed playservices signin from mainmenu screen

--- core/src/com/nitsanmichael/popping_frog_game/PoppingFrog.java
+++ core/src/com/nitsanmichael/popping_frog_game/PoppingFrog.java
@@ -52,8 +52,6 @@ public class PoppingFrog extends Game {
 		this.tweenController = new TweenController();
 		FadingScreen screen = new MainMenuScreen(PoppingFrog.this);
 		screen.fadeIn(this, 0.5f);
-
-		this.playServices.signIn();
 	}
 
 	@Override
